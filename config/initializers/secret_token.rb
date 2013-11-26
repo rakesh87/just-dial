aa3bf363e338ec7e79d727b397a8422f3214db9a
rakesh verma <rakesh.verma0487@gmail.com>
2013-11-26 20:50:27 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JustDial::Application.config.secret_token = 'c4479095d78b77b43ead7e1324ea506575f63e053b57451bae6e73e5efec111aef4d67c7dcbb94dcc9c02611e518a84eecdffca46a31db40212cdfbe8d4c0859'
