1c6184af3aa2aa36c3baa5cdb91908865fc0d18c
rakesh verma <rakesh.verma0487@gmail.com>
2013-11-30 21:06:15 +0530

    add validation to model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ActiveRecord::Base
   attr_accessible :detail, :name
 
+  validates :detail, :name, presence: true
+
   def avg_ratings
     return "no ratings available" if review_ratings.length <= 0
     review_ratings.collect(&:rating).sum.to_f/review_ratings.length
