d7fa7e3430413bb2e92110dd0a6cc53f3e58bc68
rakesh verma <rakesh.verma0487@gmail.com>
2013-12-01 00:42:16 +0530

    add specific validation on rating

--- app/models/review_rating.rb
+++ app/models/review_rating.rb
@@ -3,6 +3,10 @@ class ReviewRating < ActiveRecord::Base
   belongs_to :user
 
   validates :review, :rating, presence: true
+  validates_numericality_of :rating, only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5,
+    :message => "can only be whole number between 1 and 5.", if: ->{ self.rating.present?}
 
   def item
     Item.find(self.item_id)
